2ad2dba721a8b81071874f40856e5afa7fcf903f
JoahZhang <31657122+JoahZhang@users.noreply.github.com>
2021-11-30 13:57:49 +0800

    Add files via upload

---
+++ jdFruitShareCodes.js
@@ -0,0 +1,37 @@
+/*
+东东农场互助码
+此文件为Node.js专用。其他用户请忽略
+支持京东N个账号
+ */
+//云服务器腾讯云函数等NOde.js用户在此处填写京东东农场的好友码。
+// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
+// 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
+let FruitShareCodes = [
+  '03560b29b8cd4ac8ac7f3ddc19f02b60@e4ac53072e684d358bb62b36097b91f2@750793589a144c4c86105276552159e7',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '9e44e144fe21455d942f5a35e189771a@4b63d5adf48d4cccbb731d9fd2fd300f@afdc2ef85f424d4f9cbc90d4cd85c079',//账号二的好友shareCode，不同好友中间用@符号隔开
+]
+
+// 从日志获取互助码
+// const logShareCodes = require('./utils/jdShareCodes');
+// if (logShareCodes.FRUITSHARECODES.length > 0 && !process.env.FRUITSHARECODES) {
+//   process.env.FRUITSHARECODES = logShareCodes.FRUITSHARECODES.join('&');
+// }
+
+// 判断github action里面是否有东东农场互助码
+if (process.env.FRUITSHARECODES) {
+  if (process.env.FRUITSHARECODES.indexOf('&') > -1) {
+    console.log(`您的东东农场互助码选择的是用&隔开\n`)
+    FruitShareCodes = process.env.FRUITSHARECODES.split('&');
+  } else if (process.env.FRUITSHARECODES.indexOf('\n') > -1) {
+    console.log(`您的东东农场互助码选择的是用换行隔开\n`)
+    FruitShareCodes = process.env.FRUITSHARECODES.split('\n');
+  } else {
+    FruitShareCodes = process.env.FRUITSHARECODES.split();
+  }
+} else {
+  console.log(`由于您环境变量(FRUITSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+}
+for (let i = 0; i < FruitShareCodes.length; i++) {
+  const index = (i + 1 === 1) ? '' : (i + 1);
+  exports['FruitShareCode' + index] = FruitShareCodes[i];
+}
