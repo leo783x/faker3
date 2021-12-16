33fc31d8975f0f1a97b3eef535e92c80040ed3ad
JoahZhang <1416036922@qq.com>
2021-12-16 14:53:18 +0800

    Update jd_malls.js

--- jd_malls.js
+++ jd_malls.js
@@ -14,7 +14,7 @@ if ($.isNode()) {
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === "false")
     console.log = () => {};
   if (process.env.ACT_URL && process.env.ACT_URL === '') console.log = () => {};
-  actURL = process.env.ACT_URL || 'https://cdn.jsdelivr.net/gh/JoahZhang/mny@main/mall_active.json'
+  actURL = process.env.ACT_URL || 'https://gitee.com/hoah/mny/raw/master/mall_active.json'
 } else {
   cookiesArr = [$.getdata("CookieJD"), $.getdata("CookieJD2"), ...$.toObj($.getdata("CookiesJD") || "[]").map((item) => item.cookie)].filter((item) => !!item);
 }
